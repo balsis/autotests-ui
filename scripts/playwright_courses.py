47ab7769c3155a175ced856c0bffc17b7c7d79df
artur <artur.balsis@gmail.com>
2025-08-18 22:15:57 +0300

    Работа с авторизацией в Playwright. Практика работы с авторизацией в Playwright

--- scripts/playwright_courses.py
+++ scripts/playwright_courses.py
@@ -25,6 +25,20 @@ with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-    page.wait_for_timeout(4000)
+    courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
+    expect(courses_title).to_be_visible()
+    expect(courses_title).to_have_text("Courses")
+
+    empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_view_icon).to_be_visible()
+
+    empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(empty_view_title).to_be_visible()
+    expect(empty_view_title).to_have_text('There is no results')
+
+    empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(empty_view_description).to_be_visible()
+    expect(empty_view_description).to_have_text('Results from the load test pipeline will be displayed here')
+    page.wait_for_timeout(3000)
--- scripts/playwright_simple_registration.py
+++ scripts/playwright_simple_registration.py
@@ -2,15 +2,14 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+    page = browser.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
     email_input = page.get_by_test_id("registration-form-email-input").locator('input')
     email_input.fill("user.name@gmail.com")
 
-    username_input = page.get_by_test_id("registration-form-username-input").locator('input')
+    username_input = page.get_by_test_id("registration-form-username-input").locator(
+        'input')
     username_input.fill("username")
 
     password_input = page.get_by_test_id("registration-form-password-input").locator('input')
@@ -19,12 +18,6 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id("registration-page-registration-button")
     registration_button.click()
 
-    context.storage_state(path="browser-state.json")
-
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-    page.wait_for_timeout(4000)
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
