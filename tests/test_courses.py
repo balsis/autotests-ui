f74bcd3d819d23cb52199e40f6e803e19647a84e
artur.balsis <artur.balsis@gmail.com>
2025-08-26 17:24:37 +0300

    Практикуемся в реализации PageComponent. Реализация и использование компонента ImageUploadWidgetComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,5 +1,4 @@
 import pytest
-from playwright.sync_api import expect, Page
 
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
@@ -23,16 +22,15 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
     create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
     create_course_page.check_visible_create_course_title()
     create_course_page.check_disabled_create_course_button()
-    create_course_page.check_visible_image_preview_empty_view()
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
     create_course_page.check_visible_create_course_form(
         title="", max_score="0", min_score="0", description="", estimated_time=""
     )
     create_course_page.check_visible_exercises_title()
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
-    create_course_page.upload_preview_image(file="./testdata/files/image.png")
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    create_course_page.image_upload_widget.upload_preview_image(file="./testdata/files/image.png")
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
     create_course_page.fill_create_course_form(
         title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10",
     )
