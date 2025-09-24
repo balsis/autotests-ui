0463fb3d36923a4a933a2b4186f1a84f00e228d6
artur.balsis <artur.balsis@gmail.com>
2025-09-24 17:08:47 +0300

    Добавление видео в Allure отчет. Рефакторинг фикстур chromium_page и chromium_page_with_state

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -6,19 +6,13 @@ from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
 def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-    yield page
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
@@ -34,14 +28,10 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture(scope="function")
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json", record_video_dir='./videos')
-    context.tracing.start(screenshots=True, snapshots=True, sources=True)
-    page = context.new_page()
-    yield page
-    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
-    browser.close()
-    allure.attach.file(f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
-    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json"
+    )
