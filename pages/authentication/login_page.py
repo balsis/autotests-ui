777a51705d845d1157ceefdc164a9a44ea64729e
artur.balsis <artur.balsis@gmail.com>
2025-09-24 16:33:55 +0300

    Добавление Allure шагов в автотесты. Практика работы с allure.step

--- pages/authentication/login_page.py
+++ pages/authentication/login_page.py
@@ -1,5 +1,6 @@
 import re
 
+import allure
 from playwright.sync_api import Page, Locator, expect
 
 from components.authentication.login_form_component import LoginFormComponent
@@ -26,6 +27,7 @@ class LoginPage(BasePage):
         self.wrong_email_or_password_alert.check_visible()
         self.wrong_email_or_password_alert.check_have_text("Wrong email or password")
 
+    @allure.step("Check visible wrong email or password alert")
     def click_registration_link(self):
         self.registration_link.click()
         self.check_current_url(re.compile(".*/#/auth/registration"))
