b67c6ff516f7fb2901bec63436e20cc12950e6d4
artur.balsis <artur.balsis@gmail.com>
2025-09-25 18:14:39 +0300

    Запуск автотестов на разных браузерах

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,5 @@
 from typing import Any, Generator
 
-import allure
 import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Page, Playwright
@@ -11,9 +10,9 @@ from tools.playwright.pages import initialize_playwright_page
 from tools.routes import AppRoute
 
 
-@pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
-    yield from initialize_playwright_page(playwright, test_name=request.node.name)
+@pytest.fixture(params=settings.browsers)
+def page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
+    yield from initialize_playwright_page(playwright, test_name=request.node.name, browser_type=request.param)
 
 
 @pytest.fixture(scope="session")
@@ -35,10 +34,11 @@ def initialize_browser_state(playwright: Playwright):
     browser.close()
 
 
-@pytest.fixture
-def chromium_page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
+@pytest.fixture(params=settings.browsers)
+def page_with_state(initialize_browser_state, request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state=settings.browser_state_file
+        storage_state=settings.browser_state_file,
+        browser_type=request.param
     )
